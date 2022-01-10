ade6c2d159dd611a8acf87aef0dba03a122f810a
chrisxyq <yuanqixu@trip.com>
2022-01-10 19:37:49 +0800

    first

---
+++ src/main/java/com/example/thymeleafdemo/controller/IndexController.java
@@ -0,0 +1,33 @@
+package com.example.thymeleafdemo.controller;
+
+import com.example.thymeleafdemo.vo.UserVO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.Date;
+
+@Controller
+public class IndexController {
+    @GetMapping("/index")
+    public String index(Model model) {
+        model.addAttribute("title", "传递的title");
+        model.addAttribute("description", "传递的description");
+        model.addAttribute("keywords", "传递的keywords");
+        return "index";
+    }
+
+    @GetMapping("/basic")
+    public String basic(Model model) {
+        UserVO userVO = new UserVO();
+        userVO.setUsername("username");
+        userVO.setAge(22);
+        userVO.setIsVip(true);
+        userVO.setCreateTime(new Date());
+        userVO.setSex(1);
+        userVO.setTags(Arrays.asList("PHP", "Node"));
+        model.addAttribute("user", userVO);
+        return "basic";
+    }
+}
